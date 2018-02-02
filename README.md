292ea757f62d458827fcb36dd5d847ea14b44477
汪丰 <wangfeng1@corp.netease.com>
2018-02-02 14:28:54 +0800

    add sigle machine and muti-machine compose file

--- README.md
+++ README.md
@@ -1,70 +1,34 @@
-##  1.基本概念：
+﻿##  1.基本概念：
 
 1. Cluster:  定义一个集群
-2. Job:  定义一个Job
+2. Job:  定义一个Job(ps, worker)
+```
+cluster = tf.train.ClusterSpec({'ps': ps_spec, 'worker': worker_spec})
+```
+
 3. Task: 每个Job可以有多个task，一般一台机器上运行一个task
 
 ##  2.运行：
 
-```
-1.ps1 [10.240.209.96:8888]
 
-python src/distributed.py --job_name='ps' --task_index=0
-
-2.work1 [10.240.209.96:8888, docker]
-
-CUDA_VISIBLE_DEVICES=0 \
-python src/distributed.py --job_name='worker' --task_index=0 \
---worker_host='0.0.0.0:8888,0.0.0.0:9999' 
-
-3.work2 [10.240.209.96:9999, docker]
-
-CUDA_VISIBLE_DEVICES=1 \
-python src/distributed.py --job_name='worker' --task_index=1 \ 
---worker_host='0.0.0.0:8888,0.0.0.0:9999' 
-```
-
-## 3.通信方式探究
-### 3.1worker之间是如何通信的
 ```
 	cluster_spec = {
 		'ps': '10.240.209.96:8888', # 参数服务器
 		'worker': '10.240.209.95:8888,10.240.209.95:9999' #两个worker，每一个有一个task
 	}
 ```
+
+## 3. 测试结果
 ```
-	1. 实验1
-	# 首先启动ps
-	python src/distributed.py --job_name='ps' --task_index=0
-	
-	# 启动worker1
-	CUDA_VISIBLE_DEVICES=0 \
-	python src/distributed.py --job_name='worker' --task_index=0 \
-    --worker_host='0.0.0.0:8888,0.0.0.0:9999' 
+# 单机测试
+1个ps 与2个worker在同一台机器上
+batch_size: 100
+step: 10000
+Train time: 11.9 s
 
-	#可以看到work1在等待work2(/job:worker/replica:0/task:1)，说明还是一个同步方式
-
-	2. 实验2
-	# 首先启动ps
-	python src/distributed.py --job_name='ps' --task_index=0
-
-	# 启动worker2
-	CUDA_VISIBLE_DEVICES=1 \ 
-	python src/distributed.py --job_name=worker --task_index=1 \ 
-	--worker_host='0.0.0.0:8888,0.0.0.0:9999'
-	
-	#可以看到work2在work1(chief worker)没有初始化的情况下阻塞在prepare_or_wait_for_session等待worker1初始化session
-
-	# 此时启动worker1
-	CUDA_VISIBLE_DEVICES=0 \
-	python src/distributed.py --job_name=worker  --task_index=0 \
-    --worker_host='0.0.0.0:8888,0.0.0.0:9999'
-    
-	可以看到worker1和woker2同时开始训练， 训练完成之后worker2继续等待
-
-	# 之所以在docker中采用worker_host='0.0.0.0:8888,0.0.0.0:9999'的方式，是为了使得worker1和worker2之间能够知道彼此的存在，能够通信，同步
+# 多机测试
+ps 在一个机器上， 2个worker 在另外一个机器上
+batch_size: 100
+step: 10000
+Train time: 66.6 s
 ```
-![图1](./images/img1.png)
-![图2](./images/img2.png)
-![图3](./images/img3.png)
-![图4](./images/img4.png)
--- src/distributed.py
+++ src/distributed.py
@@ -3,21 +3,22 @@ import math
 import tempfile
 import time
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+#from tensorflow.examples.tutorials.mnist import input_data
 
 flags = tf.app.flags
 IMAGE_PIXELS = 28
-# 定义默认训练参数和数据路径
-flags.DEFINE_string('data_dir', '/tmp/mnist-data', 'Directory  for storing mnist data')
+flags.DEFINE_string('data_dir', './mnist-data', 'Directory  for storing mnist data')
 flags.DEFINE_integer('hidden_units', 100, 'Number of units in the hidden layer of the NN')
-flags.DEFINE_integer('train_steps', 1000, 'Number of training steps to perform')
+flags.DEFINE_integer('train_steps', 10000, 'Number of training steps to perform')
 flags.DEFINE_integer('batch_size', 100, 'Training batch size ')
 flags.DEFINE_float('learning_rate', 0.01, 'Learning rate')
+
 # 定义分布式参数
 # 参数服务器parameter server节点
-flags.DEFINE_string('ps_hosts', '10.240.209.96:8888', 'Comma-separated list of hostname:port pairs')
+flags.DEFINE_string('ps_hosts', '10.240.208.65:8888', 'Comma-separated list of hostname:port pairs')
 # 两个worker节点
-flags.DEFINE_string('worker_hosts', '10.240.209.95:8888,10.240.209.95:9999',
+flags.DEFINE_string('worker_hosts', '10.240.209.91:8888,10.240.209.91:9999',
                     'Comma-separated list of hostname:port pairs')
 # 设置job name参数
 flags.DEFINE_string('job_name', None, 'job name: worker or ps')
@@ -30,7 +31,7 @@ FLAGS = flags.FLAGS
 
 
 def main(unused_argv):
-    mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+    mnist = tf.contrib.learn.datasets.load_dataset('mnist')
 
     if FLAGS.job_name is None or FLAGS.job_name == '':
         raise ValueError('Must specify an explicit job_name !')
@@ -45,18 +46,16 @@ def main(unused_argv):
     worker_spec = FLAGS.worker_hosts.split(',')
 
     # 创建集群
-    num_worker = len(worker_spec)
     cluster = tf.train.ClusterSpec({'ps': ps_spec, 'worker': worker_spec})
-    server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    server = tf.train.Server(cluster, job_name=FLAGS.job_name, task_index=FLAGS.task_index, config=config)
     if FLAGS.job_name == 'ps':
         server.join()
 
     is_chief = (FLAGS.task_index == 0)
-    # worker_device = '/job:worker/task%d/cpu:0' % FLAGS.task_index
-    with tf.device(tf.train.replica_device_setter(
-            cluster=cluster
-    )):
-        global_step = tf.Variable(0, name='global_step', trainable=False)  # 创建纪录全局训练步数变量
+    with tf.device(tf.train.replica_device_setter(cluster=cluster)):
+        global_step = tf.Variable(0, name='global_step', trainable=False)
 
         hid_w = tf.Variable(tf.truncated_normal([IMAGE_PIXELS * IMAGE_PIXELS, FLAGS.hidden_units],
                                                 stddev=1.0 / IMAGE_PIXELS), name='hid_w')
@@ -67,17 +66,15 @@ def main(unused_argv):
         sm_b = tf.Variable(tf.zeros([10]), name='sm_b')
 
         x = tf.placeholder(tf.float32, [None, IMAGE_PIXELS * IMAGE_PIXELS])
-        y_ = tf.placeholder(tf.float32, [None, 10])
+        y_ = tf.placeholder(tf.int32, [None])
 
         hid_lin = tf.nn.xw_plus_b(x, hid_w, hid_b)
         hid = tf.nn.relu(hid_lin)
 
-        y = tf.nn.softmax(tf.nn.xw_plus_b(hid, sm_w, sm_b))
+        logits = tf.add(tf.matmul(hid, sm_w), sm_b) 
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_, logits=logits)
         # accuracy
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
+        accuracy = tf.metrics.accuracy(y_, tf.argmax(logits, axis=1))
 
         opt = tf.train.AdamOptimizer(FLAGS.learning_rate)
 
@@ -85,13 +82,15 @@ def main(unused_argv):
         # 生成本地的参数初始化操作init_op
         init_op = tf.global_variables_initializer()
         train_dir = tempfile.mkdtemp()
-        sv = tf.train.Supervisor(is_chief=is_chief, logdir=train_dir, init_op=init_op, recovery_wait_secs=1,
-                                 global_step=global_step)
+        # Supervisor is deprecated, using "MonitoredTrainingSession" instead
+        # logdir: checkpoint save dir
+        sv = tf.train.Supervisor(is_chief=is_chief, logdir=train_dir, init_op=init_op, recovery_wait_secs=1, global_step=global_step)
 
         if is_chief:
             print 'Worker %d: Initailizing session...' % FLAGS.task_index
         else:
             print 'Worker %d: Waiting for session to be initaialized...' % FLAGS.task_index
+
         sess = sv.prepare_or_wait_for_session(server.target)
         print 'Worker %d: Session initialization  complete.' % FLAGS.task_index
 
@@ -117,10 +116,15 @@ def main(unused_argv):
         train_time = time_end - time_begin
         print 'Training elapsed time:%f s' % train_time
 
-        val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
-        val_xent = sess.run(cross_entropy, feed_dict=val_feed)
-        val_accuracy = sess.run(accuracy, feed_dict=val_feed)
-        print 'After %d training step(s), validation cross entropy = %g, accuracy: %f' % (FLAGS.train_steps, val_xent, val_accuracy)
+        if is_chief:
+          val_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+          val_xent = sess.run(cross_entropy, feed_dict=val_feed)
+          val_accuracy = sess.run(accuracy, feed_dict=val_feed)
+          print 'After %d training step(s)' % (FLAGS.train_steps)
+          print('cross_entropy:')
+          print(np.mean(val_xent))
+          print("accuracy:")
+          print(val_accuracy)
         sess.close()
 
 if __name__ == '__main__':
